f6db8291585f551acde0988bbfe0541c0ea2da04
tediorewf <meremeev@sfedu.ru>
2022-12-19 18:18:16 +0300

    test: add factorial function test case

---
+++ FactorialUnitTest/FactorialFunctionUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Factorial.FactorialFunction;
+
+namespace FactorialUnitTest
+{
+    [TestClass]
+    public class FactorialFunctionUnitTest
+    {
+        private const double delta = 0.0001;
+
+        [TestMethod]
+        public void Test_computes_factorail_0_correctly()
+        {
+            int n = 0;
+            double factorial = ComputeFactorial(n);
+            Assert.AreEqual(1.0, factorial, delta);
+        }
+
+        [TestMethod]
+        public void Test_computes_factorial_1_correctly()
+        {
+            int n = 1;
+            double factorial = ComputeFactorial(n);
+            Assert.AreEqual(1.0, factorial, delta);
+        }
+
+        [TestMethod]
+        public void Test_computes_factorial_5_correctly()
+        {
+            int n = 5;
+            double factorial = ComputeFactorial(n);
+            Assert.AreEqual(120.0, factorial, delta);
+        }
+
+        [TestMethod]
+        public void Test_throws_argument_exception_if_n_is_negative()
+        {
+            int n = -1;
+            Assert.ThrowsException<ArgumentException>(() => ComputeFactorial(n));
+        }
+    }
+}
---
+++ FactorialUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FactorialUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FactorialUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c74e8704-b4d8-4e4a-8c20-f7bf69c06047")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
