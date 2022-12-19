889651c450380d7216b816ce238d3f971daa0a12
tediorewf <meremeev@sfedu.ru>
2022-12-19 16:46:54 +0300

    feat: add cli

---
+++ Factorial/FactorialFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Factorial
+{
+    public static class FactorialFunction
+    {
+        public static double ComputeFactorial(int n)
+        {
+            EnsureNIsNotNegative(n);
+            return ComputeFactorialRecursively(n);
+        }
+
+        private static void EnsureNIsNotNegative(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("n is negative");
+            }
+        }
+
+        private static double ComputeFactorialRecursively(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            return n * ComputeFactorialRecursively(n - 1);
+        }
+    }
+}
---
+++ Factorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static Factorial.FactorialFunction;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("n is undefined");
+                return 1;
+            }
+
+            if (!int.TryParse(args[0], out int n))
+            {
+                Console.Error.WriteLine("n is NaN");
+                return 1;
+            }
+
+            try
+            {
+                Console.WriteLine(ComputeFactorial(n));
+                return 0;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+    }
+}
